48b7740f9074fbec92573f0d687c12035cb51eb1
kkh <kkhh719@gmail.com>
2022-07-13 16:20:42 +0900

    추천메뉴에 json 활용

--- src/main/resources/static/js/best.js
+++ src/main/resources/static/js/best.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
         $.getJSON("/api/v1/best", function(result){
+            let html ="";
             $.each(result, function(i, field){
                 //$("div").append(field + " ");
-                console.log(field);
+                //console.log(field.menu);
+               // console.log(field['price']);
+                //console.log(field.img);
+                html += "<h5>" + field.menu + "</h5>"
+                html += "<h4>" + field.price + "</h4>"
+                html += "<img src='" + field.img + "'>"
             });
+            $("#bestmenu").html(html);
         });
 });
